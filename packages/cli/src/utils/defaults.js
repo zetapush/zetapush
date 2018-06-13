bd00b1b8a8e888f3f5153f304f608d20f18b53bb
Grégory Houllier <gregory.houllier@gmail.com>
2018-06-13 18:09:10 +0200

    chore(defaults): update platform url to https://celtia.zetapush.com
    affects: @zetapush/cli, @zetapush/core, @zetapush/create, @zetapush/example

--- packages/cli/src/utils/defaults.js
+++ packages/cli/src/utils/defaults.js
@@ -1,5 +1,5 @@
 module.exports = {
-  PLATFORM_URL: `https://celtia-alpha.zpush.io/zbo/pub/business`,
+  PLATFORM_URL: `https://celtia.zetapush.com/zbo/pub/business`,
   DEFAULT_NAMESPACE: ``,
   FRONT_FOLDER_PATH: `front`,
   WORKER_FOLDER_PATH: `.`,
