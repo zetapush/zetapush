d2f548a9db82ea199ce6d9dad74f1ef986990781
Grégory Houllier <gregory.houllier@gmail.com>
2018-06-19 09:31:48 +0200

    test(integration): update nominal test case
    affects: @zetapush/integration

--- packages/integration/spec/nominal.spec.js
+++ packages/integration/spec/nominal.spec.js
@@ -1,44 +1,50 @@
 const { WeakClient } = require('@zetapush/core');
 const transports = require('@zetapush/cometd/lib/node/Transports');
-const { rm, npmInit, zetaPush, readZetarc } = require('./utils/commands')
+const { rm, npmInit, zetaPush, readZetarc } = require('./utils/commands');
 const PATTERN = /Hello World from JavaScript (\d+)/;
 
-describe(`As developer with 
+describe(`As developer with
       - valid account
       - no configured application
   `, () => {
-  const projectDir = 'project-nominal-case'
+  const projectDir = 'project-nominal-case';
 
   beforeEach(async () => {
-    this.developerLogin = process.env.ZETAPUSH_DEVELOPER_LOGIN
-    this.developerPassword = process.env.ZETAPUSH_DEVELOPER_PASSWORD
-      // clean
-    await rm('.generated-projects/'+projectDir)
+    this.developerLogin = process.env.ZETAPUSH_DEVELOPER_LOGIN;
+    this.developerPassword = process.env.ZETAPUSH_DEVELOPER_PASSWORD;
+    // clean
+    await rm('.generated-projects/' + projectDir);
   });
 
-  it(`should be able to 
+  it(
+    `should be able to
       - have a new hello-world project
       - push hello-world
-      - use published hello-world custom cloud services`, async () => {
-    // 1) npm init
-    await npmInit(this.developerLogin, this.developerPassword, projectDir)
-    const zetarc = await readZetarc(projectDir)
-    expect(zetarc).toBeTruthy()
-    expect(zetarc.developerLogin).toBe(this.developerLogin)
-    expect(zetarc.developerPassword).toBe(this.developerPassword)
-    
-    // 2) zeta push
-    await zetaPush(projectDir)
+      - use published hello-world custom cloud services`,
+    async () => {
+      // 1) npm init
+      await npmInit(this.developerLogin, this.developerPassword, projectDir);
 
-    // 3) check using a client
-    this.client = new WeakClient({
-      ...zetarc,
-      transports
-    })
-    await this.client.connect();
-    const api = this.client.createProxyTaskService();
-    const message = await api.hello();
-    expect(typeof message).toBe('string');
-    expect(PATTERN.test(message)).toBe(true);
-  }, 10*60*1000);
+      // 2) zeta push
+      await zetaPush(projectDir);
+
+      let zetarc = await readZetarc(projectDir);
+      expect(zetarc).toBeTruthy();
+      expect(zetarc.appName).toBeTruthy();
+      expect(zetarc.developerLogin).toBe(this.developerLogin);
+      expect(zetarc.developerPassword).toBe(this.developerPassword);
+
+      // 3) check using a client
+      this.client = new WeakClient({
+        ...zetarc,
+        transports,
+      });
+      await this.client.connect();
+      const api = this.client.createProxyTaskService();
+      const message = await api.hello();
+      expect(typeof message).toBe('string');
+      expect(PATTERN.test(message)).toBe(true);
+    },
+    10 * 60 * 1000,
+  );
 });
--- packages/integration/spec/utils/commands.js
+++ packages/integration/spec/utils/commands.js
@@ -4,40 +4,69 @@ const fs = require('fs');
 const readFile = util.promisify(fs.readFile);
 const rimraf = require('rimraf');
 
-
-const rm = (path) => new Promise((resolve, reject) => rimraf(path, (failure) => failure ? reject(failure) : resolve()))
+const rm = (path) =>
+  new Promise((resolve, reject) =>
+    rimraf(path, (failure) => (failure ? reject(failure) : resolve())),
+  );
 
 const npmInit = (developerLogin, developerPassword, dir) => {
-  if(npmVersion().major<5) {
-    throw new Error("Minimum required npm version is 5.6.0")
+  if (npmVersion().major < 5) {
+    throw new Error('Minimum required npm version is 5.6.0');
   }
   let cmd;
-  if(npmVersion().major==5) {
-    cmd = execa('npx', ['@zetapush/create', dir, '--developer-login', developerLogin, '--developer-password', developerPassword], {cwd: '.generated-projects'});
+  if (npmVersion().major == 5) {
+    cmd = execa(
+      'npx',
+      [
+        '@zetapush/create',
+        dir,
+        '--developer-login',
+        developerLogin,
+        '--developer-password',
+        developerPassword,
+      ],
+      { cwd: '.generated-projects' },
+    );
   } else {
-    cmd = execa('npm', ['init', '@zetapush', dir, '--developer-login', developerLogin, '--developer-password', developerPassword], {cwd: '.generated-projects'});
+    cmd = execa(
+      'npm',
+      [
+        'init',
+        '@zetapush',
+        dir,
+        '--developer-login',
+        developerLogin,
+        '--developer-password',
+        developerPassword,
+      ],
+      { cwd: '.generated-projects' },
+    );
   }
   cmd.stdout.pipe(process.stdout);
   cmd.stderr.pipe(process.stdout);
   return cmd;
-}
+};
 
 const zetaPush = (dir) => {
-  const cmd = execa('npm', ['run', 'deploy', '--', '-vvv'], {cwd: '.generated-projects/'+dir});
+  const cmd = execa('npm', ['run', 'deploy', '--', '-vvv'], {
+    cwd: '.generated-projects/' + dir,
+  });
   cmd.stdout.pipe(process.stdout);
   cmd.stderr.pipe(process.stdout);
   return cmd;
-}
+};
 
 const readZetarc = async (dir) => {
-  const content = await readFile(`.generated-projects/${dir}/.zetarc`, { encoding: 'utf-8' });
+  const content = await readFile(`.generated-projects/${dir}/.zetarc`, {
+    encoding: 'utf-8',
+  });
   return JSON.parse(content);
-}
+};
 
 const npmVersion = () => {
   const { stdout } = execa.sync('npm', ['--version']);
   const [major, minor, patch] = stdout.split('.').map((v) => parseInt(v, 10));
-  return {major, minor, patch};
-}
+  return { major, minor, patch };
+};
 
-module.exports = { rm, npmInit, readZetarc, zetaPush }
+module.exports = { rm, npmInit, readZetarc, zetaPush };
