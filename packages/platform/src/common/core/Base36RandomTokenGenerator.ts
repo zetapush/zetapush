fb843ee68e1fdc3906ebdeecc608fe216d6ec6ac
Aurélien Baudet <aurelien.baudet@zetapush.com>
2018-07-06 11:20:56 +0200

    feat(tests) Prepare Unit tests and Integration tests

---
+++ packages/platform/src/common/core/Base36RandomTokenGenerator.ts
@@ -0,0 +1,23 @@
+import { TokenGenerator, Token } from '../api/Token';
+import { IllegalArgumentError } from '../api/exception';
+
+export class Base36RandomTokenGenerator implements TokenGenerator {
+  constructor(private length = 20) {
+    if (this.length <= 0) {
+      throw new IllegalArgumentError(
+        'Token size must contain at least one character',
+        'length',
+      );
+    }
+  }
+
+  generate(): Token {
+    let value = '';
+    do {
+      value += Math.random()
+        .toString(36)
+        .substr(2, 10);
+    } while (value.length < this.length);
+    return { value: value.substr(0, this.length) };
+  }
+}
