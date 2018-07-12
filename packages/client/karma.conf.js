677bd28697876c4ed01206e00787e1fc2312ff98
Grégory Houllier <gregory.houllier@gmail.com>
2018-07-12 08:50:02 +0200

    fix(test): update karma conf with the new package name
    affects: @zetapush/client

--- packages/client/karma.conf.js
+++ packages/client/karma.conf.js
@@ -14,7 +14,7 @@ module.exports = function (config) {
     // list of files / patterns to load in the browser
     files: [
       '../platform/dist/zetapush-platform.js',
-      'dist/zetapush.js',
+      'dist/zetapush-client.js',
       'test/**/*.spec.js',
     ],
 
