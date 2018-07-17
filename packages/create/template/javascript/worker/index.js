e10642efff14b9116520b580f5d795e9b8822b54
dez <dez@localhost.localdomain>
2018-07-17 14:38:12 +0200

    WIP: adding onApplicationBootstrap on templates
    affects: @zetapush/create

--- packages/create/template/javascript/worker/index.js
+++ packages/create/template/javascript/worker/index.js
@@ -1,4 +1,11 @@
+import {onApplicationBootstrap} from "@zetapush/platform";
 module.exports = class Api {
+
+  async [onApplicationBootstrap](){
+    // Init your backend here
+    return ;
+  }
+
   hello() {
     return `Hello World from JavaScript ${Date.now()}`;
   }
--- packages/create/template/typescript/worker/index.ts
+++ packages/create/template/typescript/worker/index.ts
@@ -1,4 +1,12 @@
+import {onApplicationBootstrap} from "@zetapush/platform";
 export default class Api {
+
+  async [onApplicationBootstrap](){
+    // Init your backend here
+    return ;
+  }
+
+
   hello() {
     return `Hello World from JavaScript ${Date.now()}`;
   }
