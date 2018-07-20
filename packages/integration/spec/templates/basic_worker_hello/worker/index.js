0d44ff9c969db408b8c1f9d4102402431425edba
Grégory Houllier <gregory.houllier@gmail.com>
2018-07-20 07:15:11 +0200

    style: prettify all the things
    affects: @zetapush/integration

--- packages/integration/spec/templates/basic_worker_hello/worker/index.js
+++ packages/integration/spec/templates/basic_worker_hello/worker/index.js
@@ -1,6 +1,5 @@
 module.exports = class Api {
-    hello() {
-      return `Hello World from JavaScript ${Date.now()}`;
-    }
+  hello() {
+    return `Hello World from JavaScript ${Date.now()}`;
   }
-  
+};
