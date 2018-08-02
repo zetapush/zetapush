10f33e5de10c34ab66970d9d6059373c6c92b259
Grégory Houllier <gregory.houllier@gmail.com>
2018-08-02 10:09:38 +0200

    feat(create-template-improvement): update created templates with latest practices
    affects: @zetapush/create, @zetapush/example

--- packages/create/template/javascript/worker/index.js
+++ packages/create/template/javascript/worker/index.js
@@ -1,6 +1,6 @@
 module.exports = class Api {
   async onApplicationBootstrap() {
-    
+    // Put your bootstrap logic here
   }
   hello() {
     return `Hello World from JavaScript ${Date.now()}`;
