6b30fce9f70791a8b05da054848ff359a906a195
dez <dez@localhost.localdomain>
2018-08-03 09:47:32 +0200

    WIP: fixing tests expect and fixing onApplicationBoostrap
    affects: @zetapush/troubleshooting

--- packages/troubleshooting/src/analyzer/bootstrap-issue.ts
+++ packages/troubleshooting/src/analyzer/bootstrap-issue.ts
@@ -1,15 +1,9 @@
-// ZetaPush modules
-import { todo } from '@zetapush/common';
 // Project modules
 import { ErrorAnalyzer, ErrorContextToAnalyze, ExitCode } from './error-analyzer';
-
 export class OnApplicationBoostrapErrorAnalyser extends ErrorAnalyzer {
   async getError(err: ErrorContextToAnalyze) {
-    todo(err);
-    // FIXME: add test with onApplicationBootstrap and then restore help analysis
-    return null;
     // RUN LOCAL
-    if (err.code) {
+    if (err.code === 'EBOOTFAIL') {
       err.code = ExitCode.BOOTSTRAP_01;
       return err;
     }
