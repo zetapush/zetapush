1d8f31b467cded1c9720992ef62d744e7305f46f
Damien Le Dantec <damien-le-dantec@hotmail.fr>
2018-09-04 16:43:29 +0200

    Improve the user management example

--- packages/user-management/samples/standard-user-workflow/worker/index.ts
+++ packages/user-management/samples/standard-user-workflow/worker/index.ts
@@ -1,90 +1,11 @@
 import { Module } from '@zetapush/core';
-import { StandardUserManagementModule, StandardUserWorkflow, ConfirmationUrlHttpHandler, MessageSender, Message, SentMessage, EmailSenderInjectable } from '@zetapush/user-management';
-
-// export class MyEmailSender implements MessageSender {
-
-//   async send(message: Message): Promise<SentMessage>  {
-//     console.log('Message : ', message);
-//     return { message };
-//   }
-// }
+import { StandardUserManagementModule, StandardUserWorkflow, ConfirmationUrlHttpHandler } from '@zetapush/user-management';
 
 @Module({
   imports: [StandardUserManagementModule],
   expose: {
     user: StandardUserWorkflow,
     http: ConfirmationUrlHttpHandler
-  },
-  // providers: [ {
-  //   provide: EmailSenderInjectable,
-  //   useClass: MyEmailSender
-  // }]
+  }
 })
 export default class Api {}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// @Module({
-//   imports: [StandardUserManagementModule],
-//   expose: {
-//     user: StandardUserWorkflow,
-//     http: ConfirmationUrlHttpHandler
-//   },
-//   provide: [{provide: EmailSenderInjectable, MySender}]
-// })
-// export default class Api {}
-
-// class MySender implements MessageSender {
-
-// }
-// export class MyConfigurer extends DefaultStandardUserWorkflowConfigurer {
-//   configure(env: Environment) {
-//     const default = super.configure();
-//     default.confirmation().email().sender(new MySender());
-//   }
-// }
