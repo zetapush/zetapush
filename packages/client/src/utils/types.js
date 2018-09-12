82a56828b69653a4d64807ce55252d0a07ebcf49
Grégory Houllier <gregory.houllier@gmail.com>
2018-09-12 14:59:35 +0200

    feat(timeoutify-client-request): add timeout parameter on all create<Async/Proxy>Service methods to
    affects: @zetapush/client
    
    ISSUES CLOSED: #161

---
+++ packages/client/src/utils/types.js
@@ -0,0 +1,9 @@
+const TYPE_PATTERN = /^\[object (\w+)\]$/;
+const { toString } = {};
+
+const stringify = (value) => toString.apply(value);
+
+export const getType = (value) => {
+  const [type = ''] = TYPE_PATTERN.exec(stringify(value)) || [];
+  return type;
+};
